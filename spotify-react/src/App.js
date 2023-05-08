14c13e78cf7905124e3ec391fe38dc98a4f51769
Mark <markfrazey@gmail.com>
2023-05-08 16:16:30 -0700

    Added React stuff

---
+++ spotify-react/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const CLIENT_ID = "2e7905b28fa443f3b1a6b0688cce9dd0"
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
